7e18b38435b75406a5d3be05d720276772b3add4
Kathiravan Arjunan <93112145+kat369@users.noreply.github.com>
2022-12-15 11:04:50 +0530

    Add files via upload

---
+++ src/Teacherview.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { env } from './Config';
+
+function Userview() {
+    const params = useParams()
+      console.log(params)
+
+      const [searchParams,setSearchParams] = useSearchParams()
+      console.log(searchParams)
+  
+      const [userData,setUseData] = useState({})
+
+
+      useEffect(()=>{
+        loadUser()
+      },[])
+
+      let loadUser = async ()=>{
+          try {
+          let user = await axios.get(`${env.api}/teacher/${params.id}`)
+          setUseData(user.data)
+          } catch (error) {
+            
+          }
+      }
+
+
+  return (
+    <>
+    <div>
+    <h1 style={{color:'black'}}>{userData.name}</h1>
+    <h1 style={{color:'black'}}>{userData.position}</h1>
+    <h1 style={{color:'black'}}>{userData.office}</h1>
+    <h1 style={{color:'black'}}>{userData.age}</h1>
+    <h1 style={{color:'black'}}>{userData.startDate}</h1>
+    <h1 style={{color:'black'}}>{userData.salary}</h1>
+    </div>
+    </>
+  )
+}
+
+export default Userview
